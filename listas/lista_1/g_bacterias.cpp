c3e379902276e18be5301875c0635a5dc1ac332f
Marcos V. Monteiro <98278090+MontMarcos@users.noreply.github.com>
2025-09-06 15:12:15 -0300

    Delete listas directory

--- listas/lista_1/g_bacterias.cpp
+++
@@ -1,7 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(){
-	int n, p; cin >> n; cin >> p;
-	cout << floor(log(n)/log(p)) << '\n';
-}
--- listas/lista_2/g_montanha.cpp
+++
@@ -1,8 +0,0 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-typedef vector <int> vi;
-
-int main(){
-	
-}
